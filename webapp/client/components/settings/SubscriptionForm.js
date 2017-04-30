1d96e13033c4e014e82238097c3bd0da1e97a7a3
randhir <randhir66115@gmail.com>
2017-04-30 23:57:17 +0530

    categories searchbox

--- webapp/client/components/settings/SubscriptionForm.js
+++ webapp/client/components/settings/SubscriptionForm.js
@@ -1,21 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table/index';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table/index';
 import Toggle from 'material-ui/Toggle';
 import Subheader from 'material-ui/Subheader';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
-import {Row, Col} from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 import RaisedButton from 'material-ui/RaisedButton';
-import {FormsyText, FormsyCheckbox} from 'formsy-material-ui/lib';
+import { FormsyText, FormsyCheckbox } from 'formsy-material-ui/lib';
 import ChipInput from 'material-ui-chip-input';
 import FontIcon from 'material-ui/FontIcon';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import Chip from 'material-ui/Chip'
 import IconButton from 'material-ui/IconButton';
-import {grey500, grey600, red500} from 'material-ui/styles/colors'
+import { grey500, grey600, red500 } from 'material-ui/styles/colors'
 import CategoryNotifiersDialog from './CategoryNotifiersDialog';
+import TextField from 'material-ui/TextField';
 
 class SubscriptionForm extends Component {
   constructor(props) {
@@ -66,7 +67,7 @@ class SubscriptionForm extends Component {
     if (data.private && this.state.email_ids.length < 2) {
       this.props.parentProps.actions.toggleSnackbar('There should be atleast 2 recepients for a private category.')
     } else {
-      data = {...data, email_ids: this.state.email_ids};
+      data = { ...data, email_ids: this.state.email_ids };
       this.props.parentProps.actions.createAnnouncementCategoryRequest(data);
       this.refs.category_form.reset();
     }
@@ -88,23 +89,23 @@ class SubscriptionForm extends Component {
     if (category) {
       category.subscribed_as = subscribed_as;
     }
-    const {parentProps} = this.props;
+    const { parentProps } = this.props;
     const institute_guid = parentProps.auth_user.selectedInstitute.inst_profile_guid;
     let url = `institute/${institute_guid}/category/notifiers`;
-    const data = {category_guid: category.category_guid};
+    const data = { category_guid: category.category_guid };
     parentProps.actions.toggleNotifiersDialog();
     parentProps.actions.fetchCategoryNotifersRequest(data, url);
-    this.setState({category});
+    this.setState({ category });
   }
 
   handleChipChange(email_ids) {
-    this.setState({email_ids})
+    this.setState({ email_ids })
   }
 
   renderUsersAdditionForm() {
     if (this.state.private_category) {
       return (
-        <Row style={{padding: '0 16px 10px'}}>
+        <Row style={{ padding: '0 16px 10px' }}>
           <Col xs={12}>
             <ChipInput
               fullWidth
@@ -114,7 +115,7 @@ class SubscriptionForm extends Component {
               newChipKeyCodes={[13, 32, 188]}
               style={this.styles.formField}
               onChange={(chips) => this.handleChipChange(chips)}
-              chipRenderer={({text, value, isFocused, isDisabled, handleClick, handleRequestDelete}, key) => (
+              chipRenderer={({ text, value, isFocused, isDisabled, handleClick, handleRequestDelete }, key) => (
                 <Chip
                   key={key}
                   className="chip"
@@ -144,12 +145,12 @@ class SubscriptionForm extends Component {
             onInvalid={this.disableButton.bind(this)}
             onValidSubmit={(data) => this.handleCategoryCreate(data)}
           >
-            <Row style={{padding: '0 16px 10px'}}>
+            <Row style={{ padding: '0 16px 10px' }}>
               <Col xs={4}>
                 <FormsyText
                   hintText="Category Name"
                   name="category_type"
-                  inputStyle={{boxShadow: 'none'}}
+                  inputStyle={{ boxShadow: 'none' }}
                   style={this.styles.formField}
                   required
                   autoComplete="off"
@@ -159,7 +160,7 @@ class SubscriptionForm extends Component {
                 <FormsyText
                   hintText="Category Description"
                   name="category_desc"
-                  inputStyle={{boxShadow: 'none'}}
+                  inputStyle={{ boxShadow: 'none' }}
                   style={this.styles.formField}
                   required
                   autoComplete="off"
@@ -172,24 +173,24 @@ class SubscriptionForm extends Component {
               transitionLeaveTimeout={500}>
               {this.renderUsersAdditionForm()}
             </ReactCSSTransitionGroup>
-            <Row style={{padding: '0 16px 16px'}}>
+            <Row style={{ padding: '0 16px 16px' }}>
               <Col xs={3}>
                 <RaisedButton
                   type="submit"
                   label="Create Category"
                   fullWidth={true}
                   disabled={!this.state.canSubmit}
-                  labelStyle={{fontSize: 12}}
-                  primary={true}/>
+                  labelStyle={{ fontSize: 12 }}
+                  primary={true} />
               </Col>
-              <div style={{display: 'inline-block'}}>
+              <div style={{ display: 'inline-block' }}>
                 <FormsyCheckbox
-                  style={{...this.styles.formField, marginTop: 8}}
-                  iconStyle={{width: 20, height: 20, marginRight: 10}}
+                  style={{ ...this.styles.formField, marginTop: 8 }}
+                  iconStyle={{ width: 20, height: 20, marginRight: 10 }}
                   name="private"
                   label="Make this a private category"
-                  labelStyle={{fontSize: 12, fontWeight:400, width:'100%', bottom:'2px'}}
-                  onChange={(event, isInputChecked) => this.setState({private_category: isInputChecked})}
+                  labelStyle={{ fontSize: 12, fontWeight: 400, width: '100%', bottom: '2px' }}
+                  onChange={(event, isInputChecked) => this.setState({ private_category: isInputChecked })}
                 />
               </div>
             </Row>
@@ -200,7 +201,7 @@ class SubscriptionForm extends Component {
   }
 
   toggleDeletionConfirmationDialog(deletionCategory) {
-    this.setState({deletionConfirmation: !this.state.deletionConfirmation, deletionCategory})
+    this.setState({ deletionConfirmation: !this.state.deletionConfirmation, deletionCategory })
   }
 
   handleCategoryDelete(category_guid) {
@@ -215,14 +216,14 @@ class SubscriptionForm extends Component {
     const actions = [
       <FlatButton
         label="Cancel"
-        labelStyle={{textTransform: 'capitalize'}}
+        labelStyle={{ textTransform: 'capitalize' }}
         primary={true}
         onTouchTap={() => this.toggleDeletionConfirmationDialog()}
       />,
-      <FlatButton label="Remove" style={{color: '#AE181F'}}
-                  labelStyle={{textTransform: 'capitalize'}}
-                  onTouchTap={() => this.handleCategoryDelete(deletionCategory.category_guid)}
-                  keyboardFocused={true}
+      <FlatButton label="Remove" style={{ color: '#AE181F' }}
+        labelStyle={{ textTransform: 'capitalize' }}
+        onTouchTap={() => this.handleCategoryDelete(deletionCategory.category_guid)}
+        keyboardFocused={true}
       />,
     ];
 
@@ -233,7 +234,7 @@ class SubscriptionForm extends Component {
         modal={false}
         open={this.state.deletionConfirmation}
         onRequestClose={this.toggleDeletionConfirmationDialog}
-        contentStyle={{width: '50%'}}
+        contentStyle={{ width: '50%' }}
       >
         {deletionCategory ?
           `Are you sure you want to delete ${deletionCategory.category_type}? This will delete all the previous
@@ -244,7 +245,7 @@ class SubscriptionForm extends Component {
   }
 
   renderOptions(category, subscribed_as) {
-    const {user} = this.props.parentProps.auth_user;
+    const { user } = this.props.parentProps.auth_user;
 
     if (category.creator.user_guid === user.user_guid || user.default_institute.user_institute_info[0].role === 'inst_superuser') {
       return (
@@ -254,31 +255,31 @@ class SubscriptionForm extends Component {
               <FontIcon className="material-icons" color={grey500}>more_horiz</FontIcon>
             </IconButton>
           }
-          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          menuStyle={{fontSize: 12}}
+          anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          menuStyle={{ fontSize: 12 }}
         >
           <MenuItem
-            innerDivStyle={{padding: '0 16px 0 55px', fontSize: 14}}
+            innerDivStyle={{ padding: '0 16px 0 55px', fontSize: 14 }}
             primaryText="Settings"
             onTouchTap={() => this.toggleCategoryNotifiersDialog(category, subscribed_as)}
             leftIcon={
               <FontIcon
                 className="material-icons"
-                style={{fontSize: 22}}
+                style={{ fontSize: 22 }}
                 color={grey500}>
                 settings
               </FontIcon>
             }
           />
           <MenuItem
-            innerDivStyle={{padding: '0 16px 0 55px', fontSize: 14}}
+            innerDivStyle={{ padding: '0 16px 0 55px', fontSize: 14 }}
             primaryText="Delete"
             onTouchTap={() => this.toggleDeletionConfirmationDialog(category)}
             leftIcon={
               <FontIcon
                 className="material-icons"
-                style={{fontSize: 22}}
+                style={{ fontSize: 22 }}
                 color={grey500}>
                 delete_forever
               </FontIcon>
@@ -297,7 +298,7 @@ class SubscriptionForm extends Component {
 
   renderCategories() {
     const allCategories = this.props.parentProps.auth_user.selectedInstitute.categories;
-    const {subscriptions, notifying_categories} = this.props.parentProps.auth_user.selectedInstitute;
+    const { subscriptions, notifying_categories } = this.props.parentProps.auth_user.selectedInstitute;
     let category_list = [];
 
     allCategories.map((category, index) => {
@@ -325,23 +326,23 @@ class SubscriptionForm extends Component {
       disabled = disabled || category.private;
 
       category_list.push(
-        <TableRow key={index} style={{height: 55}}>
-          <TableRowColumn style={{textTransform: 'capitalize', lineHeight: '16px'}}>
-            {category.category_type} {category.private ? <span style={this.styles.privateLabel}>Private</span> : <span/>}
-            <p style={{margin: '4px 0', fontSize: 'smaller', color: grey500}}>
+        <TableRow key={index} style={{ height: 55 }}>
+          <TableRowColumn style={{ textTransform: 'capitalize', lineHeight: '16px' }}>
+            {category.category_type} {category.private ? <span style={this.styles.privateLabel}>Private</span> : <span />}
+            <p style={{ margin: '4px 0', fontSize: 'smaller', color: grey500 }}>
               {category.creator.first_name} {category.creator.last_name}
             </p>
           </TableRowColumn>
           <TableRowColumn>
             {subscribed_as}
-            <p style={{margin: '4px 0', fontSize: 'smaller', color: grey500}}>
+            <p style={{ margin: '4px 0', fontSize: 'smaller', color: grey500 }}>
               {category.subscribers_count} {category.subscribers_count === 1 ? 'subscriber' : 'subscribers'}
             </p>
           </TableRowColumn>
           <TableRowColumn>
             <Toggle disabled={disabled} defaultToggled={toggle}
-                    data-toggled={toggle}
-                    onToggle={(evt) => this.handleToggle(evt, category.category_guid)}/>
+              data-toggled={toggle}
+              onToggle={(evt) => this.handleToggle(evt, category.category_guid)} />
           </TableRowColumn>
           {
             this.props.showOptions ? (
@@ -363,26 +364,30 @@ class SubscriptionForm extends Component {
       <div>
         {this.renderCreateCategoryForm()}
         <Subheader>Subscriptions Manager</Subheader>
-        <div style={{padding: '0 16px 25px'}}>
+        <div style={{ padding: '0 16px 25px' }}>
+          <TextField
+            hintText="Search Category"
+            fullWidth={true}
+          />
           <Table fixedHeader={true} selectable={false}>
             <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
               <TableRow>
-                <TableHeaderColumn style={{color: grey600}}>
+                <TableHeaderColumn style={{ color: grey600 }}>
                   Category
-                  <p style={{margin: '4px 0', fontSize: 'smaller', color: grey500}}>
+                  <p style={{ margin: '4px 0', fontSize: 'smaller', color: grey500 }}>
                     Category Creator
                   </p>
                 </TableHeaderColumn>
-                <TableHeaderColumn style={{color: grey600}}>
+                <TableHeaderColumn style={{ color: grey600 }}>
                   Subscribed As
-                  <p style={{margin: '4px 0', fontSize: 'smaller', color: grey500}}>
+                  <p style={{ margin: '4px 0', fontSize: 'smaller', color: grey500 }}>
                     Number of subscribers
                   </p>
                 </TableHeaderColumn>
-                <TableHeaderColumn style={{color: grey600}}>Status</TableHeaderColumn>
+                <TableHeaderColumn style={{ color: grey600 }}>Status</TableHeaderColumn>
                 {
                   this.props.showOptions ?
-                    (<TableHeaderColumn style={{color: grey600}}>Actions</TableHeaderColumn>) :
+                    (<TableHeaderColumn style={{ color: grey600 }}>Actions</TableHeaderColumn>) :
                     null
                 }
               </TableRow>
