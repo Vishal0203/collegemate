4db3825e140a41aa48758541efcd2e83debfbd0b
Aravind Balla <bsaaravind@gmail.com>
2017-09-12 23:37:24 +0530

    indentation fixes

--- server/app/Http/Controllers/UserProjectController.php
+++ server/app/Http/Controllers/UserProjectController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-
 use App\UserProject;
+
 use Illuminate\Http\Request;
 
 class UserProjectController
--- webapp/client/components/settings/AddProjectDialog.js
+++ webapp/client/components/settings/AddProjectDialog.js
@@ -9,99 +9,99 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
 export default class AddProjectDialog extends React.Component {
-    state = {
-        open: false,
-    };
+  state = {
+    open: false,
+  };
 
-    get styles() {
-        return {
-            formField: {
-                fontSize: 14,
-                fontWeight: 300,
-                width: '100%',
-            },
-            floatingLabelStyle: {
-                fontSize: 16
-            },
-            alert: {
-                backgroundColor: 'transparent',
-                textAlign: 'center',
-                color: 'rgb(167, 26, 26)',
-                fontSize: 'large',
-                fontWeight: '500'
-            },
-            fab: {
-                float: 'right'
-            }
-        }
+  get styles() {
+    return {
+      formField: {
+        fontSize: 14,
+        fontWeight: 300,
+        width: '100%',
+      },
+      floatingLabelStyle: {
+        fontSize: 16
+      },
+      alert: {
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+        color: 'rgb(167, 26, 26)',
+        fontSize: 'large',
+        fontWeight: '500'
+      },
+      fab: {
+        float: 'right'
+      }
     }
+  }
 
-    handleOpen = () => {
-        this.setState({open: true});
-    };
+  handleOpen = () => {
+    this.setState({open: true});
+  };
 
-    handleClose = () => {
-        this.setState({open: false});
-    };
+  handleClose = () => {
+    this.setState({open: false});
+  };
 
-    render() {
-        const actions = [
-            <FlatButton
-                label="Cancel"
-                primary={true}
-                onClick={this.handleClose}
-            />,
-            <FlatButton
-                label="Add"
-                primary={true}
-                keyboardFocused={true}
-                onClick={this.handleClose}
-            />,
-        ];
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Add"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.handleClose}
+      />,
+    ];
 
-        return (
-            <div>
-                <FloatingActionButton style={this.styles.fab} label="Dialog" onClick={this.handleOpen}>
-                    <ContentAdd />
-                </FloatingActionButton>
-                <Dialog
-                    title="Add Project"
-                    actions={actions}
-                    modal={false}
-                    open={this.state.open}
-                    onRequestClose={this.handleClose}
-                >
-                <Formsy.Form>
-                    <Row>
-                        <Col xs={12}>
-                          <FormsyText
-                            floatingLabelText="Title"
-                            floatingLabelStyle={this.styles.floatingLabelStyle}
-                            name="title"
-                            textareaStyle={{boxShadow: 'none'}}
-                            style={this.styles.formField}
-                            fullWidth={true}
-                            required
-                          />
-                        </Col>
-                        <Col xs={12}>
-                          <FormsyText
-                            floatingLabelText="Description"
-                            floatingLabelStyle={this.styles.floatingLabelStyle}
-                            name="description"
-                            textareaStyle={{boxShadow: 'none'}}
-                            style={this.styles.formField}
-                            multiLine={true}
-                            fullWidth={true}
-                            rows={1}
-                            rowsMax={4}
-                            required
-                          />
-                        </Col>
-                    </Row>
-                </Formsy.Form>
-                </Dialog>
-            </div>
-        );
-    }
+    return (
+        <div>
+          <FloatingActionButton style={this.styles.fab} label="Dialog" onClick={this.handleOpen}>
+            <ContentAdd />
+          </FloatingActionButton>
+          <Dialog
+            title="Add Project"
+            actions={actions}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+          >
+            <Formsy.Form>
+              <Row>
+                <Col xs={12}>
+                  <FormsyText
+                    floatingLabelText="Title"
+                    floatingLabelStyle={this.styles.floatingLabelStyle}
+                    name="title"
+                    textareaStyle={{boxShadow: 'none'}}
+                    style={this.styles.formField}
+                    fullWidth={true}
+                    required
+                  />
+                </Col>
+                <Col xs={12}>
+                  <FormsyText
+                    floatingLabelText="Description"
+                    floatingLabelStyle={this.styles.floatingLabelStyle}
+                    name="description"
+                    textareaStyle={{boxShadow: 'none'}}
+                    style={this.styles.formField}
+                    multiLine={true}
+                    fullWidth={true}
+                    rows={1}
+                    rowsMax={4}
+                    required
+                  />
+                </Col>
+              </Row>
+            </Formsy.Form>
+          </Dialog>
+        </div>
+    );
+  }
 }
